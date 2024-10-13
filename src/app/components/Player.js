d5a678ce2baada8de4f10c949e9949108d941734
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-10-13 12:19:39 +0100

    new commits

--- src/app/components/Player.js
+++ src/app/components/Player.js
@@ -7,7 +7,28 @@ const Player = ({ currentSong, isPlaying, setIsplaying }) => {
         setIsplaying(!isPlaying)
     }
 
-  return <div>Player</div>;
+    return (<div className="fixed bottom-0 w-full p-4 bg-gray-500 text-white">
+        (
+            currentSong ? (
+                <>
+                    <Howler
+                        src={currentSong.url}
+                        playing={isPlaying}
+                        ref={playerRef}
+                        onEnd={() => setIsplaying(false)} />
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <p className="font-bold">{ currentSong.title}</p>
+                            <p className="text-sm text-gray-400">{ currentSong.artist}</p>
+                        </div>
+                        <button className="px-4 py-2 font-bold text-white bg-green-500 rounded-full" onClick={handlePlayPause}>{ isPlaying? 'Pause' : 'Play'}</button>
+                    </div>
+                </>
+            )
+    )
+    </div>
+    
+    )
 };
 
 export default Player;
