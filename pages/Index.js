081928c9eda77e464d1fe18969962414d5491127
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-10-14 16:43:45 +0100

    new commits

---
+++ pages/Index.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import PlayList from "../src/app/components/PlayList";
+import Player from "../src/app/components/Player";
+
+export default function Home() {
+  const [songs, setSongs] = useState([]);
+    const [currentSong, setCurrentSongs] = useState(null);
+    const [isPlaying, setIsplaying] = useState(false);
+
+
+    useEffect(() => {
+        const fetchSongs = async () => {
+            const res = await fetch('api/songs');
+            const { data } = await res.json()
+            setSongs(data)
+        }
+        fetchSongs();
+    }, [])
+    
+    return (
+        <div className="min-h-screen bg-gray-100">
+            <h1 className="p-2 text-2xl font-bold text-center">Music PLayer</h1>
+            <PlayList songs={songs} setCurrentSongs={setCurrentSongs}/>
+            <Player currentSong={currentSong} isPlaying={isPlaying} setIsplaying={}/>
+        </div>
+    )
+}
